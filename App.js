c62b0f3cc5e047f8739847f3d319263180b47bea
Jose Luis Lopez Bellido <joseluislobell@gmail.com>
2022-07-20 14:50:11 +0200

    Add logout fucntionality and button

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
 import { useContext } from 'react'
 
+import IconButton from './components/ui/IconButton'
 import LoginScreen from './screens/LoginScreen'
 import SignupScreen from './screens/SignupScreen'
 import WelcomeScreen from './screens/WelcomeScreen'
@@ -29,6 +30,7 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+    const authCxt = useContext(AuthContext)
     return (
         <Stack.Navigator
             screenOptions={{
@@ -37,7 +39,20 @@ function AuthenticatedStack() {
                 contentStyle: { backgroundColor: Colors.primary100 },
             }}
         >
-            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen
+                name="Welcome"
+                component={WelcomeScreen}
+                options={{
+                    headerRight: ({ tintColor }) => (
+                        <IconButton
+                            icon="exit"
+                            color={tintColor}
+                            size={24}
+                            onPress={authCxt.logout}
+                        />
+                    ),
+                }}
+            />
         </Stack.Navigator>
     )
 }
